170004acaf791ef469000294adfc83b51e4751ae
icebeal <darrenthiores@gmail.com>
2021-09-28 11:08:47 +0700

    changing setName value

--- app/src/main/java/com/icebeal/cleanarchitecture/presenter/MainActivity.kt
+++ app/src/main/java/com/icebeal/cleanarchitecture/presenter/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         val factory = ViewModelFactory.getInstance()
         mainViewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
 
-        mainViewModel.setName("Darren")
+        mainViewModel.setName("Yaya")
         mainViewModel.message.observe(this, {
 
             binding?.text?.text = it.message
